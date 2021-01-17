f6934dd8115a72add851d122265064fd24c11bed
Ian Bruns <ian@rbruns.com>
2021-01-17 16:23:07 -0700

    created makeExercisesArray()

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -46,7 +46,43 @@ function makeRoutinesArray(users) {
     ]
 }
 
+function makeExercisesArray(routines) {
+    return [
+        {
+            exercises_id: 1,
+            exercises_name: 'Exercise-1',
+            exercises_description: 'Description-1',
+            assigned_routine: routines[0],
+        },
+        {
+            exercises_id: 2,
+            exercises_name: 'Exercise-2',
+            exercises_description: 'Description-2',
+            assigned_routine: routines[1],
+        },
+        {
+            exercises_id: 3,
+            exercises_name: 'Exercise-3',
+            exercises_description: 'Description-3',
+            assigned_routine: routines[1],
+        },
+        {
+            exercises_id: 4,
+            exercises_name: 'Exercise-4',
+            exercises_description: 'Description-4',
+            assigned_routine: routines[0],
+        },
+        {
+            exercises_id: 5,
+            exercises_name: 'Exercise-5',
+            exercises_description: 'Description-5',
+            assigned_routine: routines[0],
+        },
+    ]
+}
+
 module.exports = {
     makeUsersArray,
     makeRoutinesArray,
+    makeExercisesArray,
 }
