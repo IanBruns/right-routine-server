014c59ae5bec6ea134dcbb40f17c49e67aee8995
Ian Bruns <ian@rbruns.com>
2021-01-17 18:47:28 -0700

    starting frest on user authentication

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const authRouter = require('./auth/auth-router');
+// const authRouter = require('./auth/auth-router');
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use('/api/auth', authRouter);
+// app.use('/api/auth', authRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
