42b09da7fbde883b14709bae660103de255c53e3
Ian Bruns <ian@rbruns.com>
2021-01-18 19:52:03 -0700

    GET code removed

--- src/routines/routines-router.js
+++ src/routines/routines-router.js
@@ -9,10 +9,6 @@ const jsonBodyParser = express.json();
 routinesRouter.route('/')
     .get(requireAuth, (req, res, next) => {
         RoutinesService.getUserRoutines(req.app.get('db'), req.user.id)
-            // .then(routines => {
-            //     const filteredRoutines = routines.filter(routine => routine.assigned_user == req.user.id);
-            //     return filteredRoutines;
-            // })
             .then(routines => {
                 res.json(routines.map(RoutinesService.serializeRoutine));
             })
