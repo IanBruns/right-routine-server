efbb3ecd19e0a9c6b3539e2ea4e0d0e4a35f71a8
Ian Bruns <ian@rbruns.com>
2021-01-19 22:02:28 -0700

    setting up exercise checkpoints

---
+++ test/exercises-endpoints.spec.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const knex = require('knex');
+const supertest = require('supertest');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+describe(`Routines Endpoints`, function () {
+    let db;
+
+    const { testUsers, testRoutines, testExercises } = helpers.makeExercisesFixtures();
+    const testUser = testUsers[0];
+    const testRoutine = testRoutines[0];
+    const textExercise = testExercises[0];
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        });
+        app.set('db', db);
+    });
+});
